896c73bba2d5f7d1218b6eb48720166cfa967aed
Jairus Ilacio <ilaciojairusmiguel@gmail.com>
2024-04-10 23:17:18 +0800

    nacorrupt

--- Inventory-System/src/main/java/Main/InventorySystem.java
+++ Inventory-System/src/main/java/Main/InventorySystem.java
@@ -1,28 +1,34 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package Main;
-import settings.progressScreen;
 
-/**
- *
- * @author jairus
- */
+import java.sql.*;
+import otherForms.*;
+
 public class InventorySystem {
 
     public static void main(String[] args) {
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                
-                progressScreen loading = new progressScreen(null,true);
+
+                progressScreen loading = new progressScreen(null, true);
                 loading.setVisible(true);
-                
-                logIn a = new logIn();
-                     a.setVisible(true);
-                
-                
+
+                new logIn().setVisible(true);
+
             }
         });
     }
+
+    public static Connection getDbCon() {
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/inventory_system", "root", "#MySQL2220");
+            return con;
+        } catch (Exception e) {
+            System.out.println(e);
+            return null;
+        }
+
+    }
+
 }
--- Inventory-System/src/main/java/otherForms/progressScreen.java
+++ Inventory-System/src/main/java/otherForms/progressScreen.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JDialog.java to edit this template
  */
-package settings;
+package otherForms;
 
 import java.awt.Color;
 import javax.swing.JDialog;
@@ -40,7 +40,6 @@ public class progressScreen extends javax.swing.JDialog {
         lblStatus = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
-        setMaximumSize(new java.awt.Dimension(550, 350));
         setMinimumSize(new java.awt.Dimension(550, 350));
         setUndecorated(true);
         setResizable(false);
